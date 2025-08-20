e79dc243c6bd5ec6786f904e7439b194afcb62b6
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2025-08-20 20:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
 class Person {
-    constructor(name, age) {
-        this._name = name;
-        this._age = age;
-    }
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set age(newAge) {
-        if (newAge > 0) {
-            this._age = newAge;
-        } else {
-            console.log("Age must be a positive number.");
-        }
-    }
+  get age() {
+    return this._age;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-    constructor(name, age) {
-        super(name, age);
-    }
-
-    study() {
-        console.log(`${this.name} is studying.`);
-    }
+  study() {
+    console.log(`${this._name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-    constructor(name, age) {
-        super(name, age);
-    }
-
-    teach() {
-        console.log(`${this.name} is teaching.`);
-    }
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
 }
 
+
+// Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
