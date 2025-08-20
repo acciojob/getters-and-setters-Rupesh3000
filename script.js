a4380ca123a7299b95c7833366fef8a5b9a983e8
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2025-08-20 20:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Student extends Person {}
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    set age(newAge) {
+        if (newAge > 0) {
+            this._age = newAge;
+        } else {
+            console.log("Age must be a positive number.");
+        }
+    }
+}
 
-// Do not change the code below this line
+class Student extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    study() {
+        console.log(`${this.name} is studying.`);
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    teach() {
+        console.log(`${this.name} is teaching.`);
+    }
+}
+
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
